ea76e33dbe63c419d9a2299afac9f4164b808996
luisarsilva <luisarsilva26@gmail.com>
2026-02-18 12:06:50 -0300

    primeiro commit

---
+++ Principal.java
@@ -0,0 +1,12 @@
+package aulaJPA.aula01;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// esse codigo printa o texto na tela
+		System.out.println("Luisa");
+	}
+
+
+}
---
+++ Somador.java
@@ -0,0 +1,28 @@
+package aulaJPA.aula01;
+
+import java.util.Scanner;
+
+public class Somador {
+
+	public static void main(String[] args) {
+		//Variáveis
+		double a,b,som;
+		//Instaciar classe Scanner
+		Scanner ler = new Scanner (System.in);
+		
+		//Entrada de dados
+		System.out.println("Informe valor 1:");
+		a = ler.nextDouble();
+		
+		System.out.println("Informe valor 2:");
+		b = ler.nextDouble();
+		
+		//Processamento
+		som = a+b;
+		//Saída
+		System.out.println("O resultado é :  "+ som);
+		
+
+	}
+
+}
